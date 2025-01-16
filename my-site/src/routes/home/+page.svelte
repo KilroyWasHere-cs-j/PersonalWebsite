50f06f762b8e5d403fe135eb32dcff87016696d9
Gabriel <gmtower1@gmail.com>
2025-01-16 10:58:16 -0500

    The triangle? ?

--- my-site/src/routes/home/+page.svelte
+++ my-site/src/routes/home/+page.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
     import NavBar from '../../components/navbar.svelte';
-    import PurTri from '../../PurpleTriangle.svg';
+    import PurTri from '../../images/PurpleTri.png';
 </script>
 
 <NavBar />
-<div class="flex flex-col justify-start w-screen h-screen text-white bg-black">
-    <div class="flex justify-center w-full h-full relative">
-        <svg id="eGPW2wz6Dek1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
-            <defs>
-                <radialGradient id="eGPW2wz6Dek2-fill" cx="0" cy="0" r="0.704765" spreadMethod="pad" gradientUnits="objectBoundingBox" gradientTransform="translate(0.5 0)">
-                    <stop id="eGPW2wz6Dek2-fill-0" offset="0%" stop-color="#7d359c"/>
-                    <stop id="eGPW2wz6Dek2-fill-1" offset="100%" stop-color="#fff"/>
-                </radialGradient>
-            </defs>
-            <path d="M20.041941,0L150,230.437388L279.958058,0L20.041941,0Z" transform="translate(.000003 0)" fill="url(#eGPW2wz6Dek2-fill)" stroke="#7d359c" stroke-width="0.6"/>
-            <path d="" fill="none" stroke="#3f5787" stroke-width="0.6"/>
-        </svg>
-        <div class="text-black text-4xl absolute top-[8rem] left-[25rem]p-4">
-            <h1>We are Voodu Software</h1>
+<div class="flex flex-col justify-start w-screen h-screen space-y-4 text-white" style="
+background-image: url({PurTri});
+background-size: cover;
+background-position: center;
+background-attachment: fixed;">
+    <div class="flex flex-col space-y-6">
+        <h1 class="text-6xl text-white m-4 motion-opacity-in-0 motion-delay-500 motion-duration-[2s]">Voodu Software.</h1>
+        <p class="text-2xl xl:w-[500px] h-full m-4 motion-opacity-in-0 motion-delay-500 motion-duration-[15s]">Lorem ipsum odor amet, consectetuer adipiscing elit. Et sem in luctus montes lacus mi enim vestibulum. Himenaeos dictum ad inceptos fermentum class amet maximus. Montes mi enim luctus nec, curae nisl efficitur. 
+            Cursus blandit maecenas semper in consectetur aenean montes ut. Consequat inceptos cras dis fames cursus ad.
+        </p>
+        <div class="motion-opacity-in-0 motion-delay-500 motion-duration-[20s]">
+            <button class="bg-white w-28 text-black rounded-md p-2 motion-bg-loop-purple-500 ml-4 hover:bg-purple-600 hover:scale-105">Work with us</button>
         </div>
-    </div>    
+    </div>
+    <div class="flex justify-end space-y-6">
+        <div class="">
+            <h1 class="text-6xl text-white m-4 motion-opacity-in-0 motion-delay-500 motion-duration-[2s]">Voodu Software.</h1>
+            <p class="text-2xl xl:w-[500px] h-full m-4 motion-opacity-in-0 motion-delay-500 motion-duration-[8s]">Lorem ipsum odor amet, consectetuer adipiscing elit. Et sem in luctus montes lacus mi enim vestibulum. Himenaeos dictum ad inceptos fermentum class amet maximus. Montes mi enim luctus nec, curae nisl efficitur. 
+                Cursus blandit maecenas semper in consectetur aenean montes ut. Consequat inceptos cras dis fames cursus ad.
+            </p>
+        </div>
+    </div>
 </div>
